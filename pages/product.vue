4a37d00e6d78a25ec3ba906d20ff7a5fea315e34
McPeverick <georg.methner@gmail.com>
2023-05-15 14:08:39 +0200

    added product page and id query

--- pages/product.vue
+++ pages/product.vue
@@ -1,4 +1,26 @@
+<script lang="ts" setup>
+    import {Product} from "@shopware-pwa/commons";
+    import {getProduct, getProducts, setup} from "@shopware-pwa/shopware-6-client";
+
+    const product = ref<Product>()
+    const id = useRoute().query.id!
+
+    onMounted(async () => {
+        setup({
+            endpoint: "https://sw.blyssum.com",
+            accessToken: "SWSCN0JZTDGZWJBNNXI2CEZNQG",
+        });
+        const productsResponse = await getProduct(id.toString());
+
+        console.log(id);
+
+        console.log(product.value)
+
+    })
+
+</script>
+
 <template>
 
-<div>test</div>
+    <div>{{ id }}</div>
 </template>
