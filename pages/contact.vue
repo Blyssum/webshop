8f583d7913a94872ca3cfab80799c30a259d20d9
McPeverick <georg.methner@gmail.com>
2023-07-01 17:59:45 +0200

    fast fertig alles gemacht

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,7 +7,7 @@
     <div class="text-white font-serif p-5 border-x-2 text-lg w-full">
         <div class="mx-auto w-full">Trade name: BLYSSUM</div>
 
-        <div class="mt-5 mx-auto w-full">Email: naseumschlag69@gmail.com</div>
+        <div class="mt-5 mx-auto w-full">Email: blyssumclothing@gmail.com</div>
 
         <div class="mt-5 mx-auto w-full">Anschrift: BLYSSUM, Methner, Klever Straße 54, 40477, Düsseldorf</div>
     </div>
