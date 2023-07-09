a55f2a338e6a5bb94ea1ed8175df8668999ed853
georg <georg.methner@gmail.com>
2023-07-09 16:06:22 +0200

    descirprion fix again

--- components/product-card.vue
+++ components/product-card.vue
@@ -65,7 +65,7 @@ const addToCartProxy = async () => {
                     <div class="inline-flex">
                         <div class="font-bold text-2xl">{{ product?.translated.name}}</div>
 
-                        <div class="ml-auto">{{ product?.calculatedPrice.totalPrice * count }}0€</div>
+                        <div class="ml-auto">{{ (product?.calculatedPrice.totalPrice * count).toString().substring(0, 4) }}0€</div>
                     </div>
 
                     <div class="text-base mt-3 text-white filter invert" v-html="product?.translated.description"></div>
